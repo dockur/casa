6d00e0de2bb197ba10cf161d3b6654fc16f431b8
Kroese <kroese@users.noreply.github.com>
2024-11-10 13:03:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,6 @@ if [ ! -S /var/run/docker.sock ]; then
   echo "ERROR: Docker socket is missing? Please bind /var/run/docker.sock in your compose file." && exit 13
 fi
 
-: "${REF_NET:="meta"}"
-
-if ! docker network inspect "$REF_NET" &>/dev/null; then
-  if ! docker network create --driver=bridge --subnet="10.21.0.0/16" "$REF_NET" >/dev/null; then
-    echo "ERROR: Failed to create network '$REF_NET'!" && exit 14
-  fi
-  if ! docker network inspect "$REF_NET" &>/dev/null; then
-    echo "ERROR: Network '$REF_NET' does not exist?" && exit 15
-  fi
-fi
-
 target=$(hostname)
 
 if ! docker inspect "$target" &>/dev/null; then
@@ -23,14 +12,6 @@ if ! docker inspect "$target" &>/dev/null; then
 fi
 
 resp=$(docker inspect "$target")
-network=$(echo "$resp" | jq -r '.[0].NetworkSettings.Networks["$REF_NET"]')
-
-if [ -z "$network" ] || [[ "$network" == "null" ]]; then
-  if ! docker network connect "$REF_NET" "$target"; then
-    echo "ERROR: Failed to connect container to network '$REF_NET'!" && exit 17
-  fi
-fi
-
 mount=$(echo "$resp" | jq -r '.[0].Mounts[] | select(.Destination == "/DATA").Source')
 
 if [ -z "$mount" ] || [[ "$mount" == "null" ]] || [ ! -d "/DATA" ]; then
@@ -46,6 +27,13 @@ fi
 
 if [[ "$mount" != "/"* ]]; then
   echo "ERROR: Please bind the /DATA folder to an absolute path!" && exit 19
+fi
+
+# Mirror external folder to local filesystem
+if [[ "$mount" != "/DATA" ]]; then
+  mkdir -p "$mount"
+  rm -rf "$mount"
+  ln -s /DATA "$mount"
 fi
 
 export DATA_ROOT="$mount"
